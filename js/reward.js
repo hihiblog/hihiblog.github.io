4e57f3147615f22a105250dee7100c768521dc7f
mcj8089 <mcj8089@163.com>
2026-02-28 16:31:10 +0800

    Deploy

---
+++ js/reward.js
@@ -0,0 +1,29 @@
+function ZanShang(){
+  this.popbg = $('.zs-modal-bg');
+  this.popcon = $('.zs-modal-box');
+  this.closeBtn = $('.zs-modal-box .close');
+  this.zsbtn = $('.zs-modal-btns .btn');
+  this.zsPay = $('.zs-modal-pay');
+  this.zsBtns = $('.zs-modal-btns');
+  this.zsFooter = $('.zs-modal-footer');
+  var that = this;
+  $('.show-zs').on('click',function(){
+    // back to top
+    that._scrollTop();
+  })
+}
+ZanShang.prototype._scrollTop = function(){
+  $('html,body').animate({scrollTop:0},500);
+}
+ZanShang.prototype._hide = function(){
+  this.popbg.hide();
+  this.popcon.hide();
+}
+ZanShang.prototype._show = function(){
+  this.popbg.show();
+  this.popcon.show();
+  this.zsBtns.show();
+  this.zsFooter.show();
+  this.zsPay.hide();
+}
+var zs = new ZanShang();
